c421940c712dd9dd8aa2184b42a8b456becb1ca4
Yassien <11986226+yassienw@users.noreply.github.com>
2018-12-15 22:52:22 +0200

    Added receivedprivatemsg and privateTyping events
    kickedBy variable of userleftgroup now returns the user's display name, or null

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -63,24 +63,35 @@ module.exports = (data, client) => {
             let user = client.users.find((user) => {return user.jid === data.find("message").attrs.from})
 
             if(data.find("body")){
-                client.emit("receivedgroupmsg", group, user, data.find("body").string)
+                client.emit("receivedgroupmsg", group, user, data.find("body").text)
             }else if(data.find("is-typing")){
                 client.emit("grouptyping", group, user, data.find("is-typing").attrs.val === "true")
             }else if(data.find("status")){
-                let status = data.find("status").text
+                let status = data.find("status")
 
-                if(status.includes("left") || status.includes("removed")){
-                    user = client.users.find((user) => {return user.jid === data.find("status").attrs.jid})
+                if(status.text.includes("left") || status.text.includes("removed")){
+                    user = client.users.find((user) => {return user.jid === status.attrs.jid})
+                    let kickedBy = (status.text.includes("removed")? status.text.split("has")[0].trim() : null)
 
-                    client.emit("userleftgroup", group, user, null)
+                    client.emit("userleftgroup", group, user, kickedBy)
                 }else if(status.includes("joined")){
                     client.getJidInfo(data.find("status").attrs.jid)
 
-                    //i can't return user info since i have to getJidInfo first for a user that just joined, return the
-                    //JID until i find a way
+                    //i can't return user info since i have to recieve the getJidInfo response first for a user that just joined,
+                    //return the JID until i find a way
                     client.emit("userjoinedgroup", group, data.find("status").attrs.jid, null)
                 }
             }
+        }else if(type === "chat"){
+            if(data.find("xiphias-mobileremote-call")){
+                //safetynet message
+            }else{
+                let user = client.friends.find((friend) => {return friend.jid === data.find("message").attrs.from})
+                client.emit("receivedprivatemsg", user, data.find("body").text)
+            }
+        }else if(type === "is-typing"){
+            let user = client.friends.find((friend) => {return friend.jid === data.find("message").attrs.from})
+            client.emit("privateTyping", user, data.find("is-typing").attrs.val === "true")
         }
     }else{
         console.log("Unhandled")

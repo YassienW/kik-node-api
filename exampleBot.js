2ad0077ca1673a5df97066e005d8b28bc6af2f2e
Yassien <11986226+yassienw@users.noreply.github.com>
2018-12-22 18:08:12 +0200

    Updated example bot with callbacks

--- exampleBot.js
+++ exampleBot.js
@@ -9,20 +9,34 @@ Kik = new KikClient({
     trackFriendInfo: true
 })
 Kik.on("authenticated", () => {
-    console.log("Authenticated boi")
+    //this is not needed since the client grabs roster automatically once authenticated
+/*    Kik.getRoster((groups, friends) => {
+        console.log("We got the roster")
+        console.log(groups)
+        console.log(friends)
+    })*/
 })
+//alternatively you can use this event for roster
 Kik.on("receivedroster", (groups, friends) => {
-    console.log("We got the roster")
-    console.log(groups)
-    console.log(friends)
+    //do stuff here
 })
 Kik.on("receivedjidinfo", (users) => {
     console.log("We got peer info")
 })
-
 //group events
 Kik.on("receivedgroupmsg", (group, sender, msg) => {
+    Kik.getJidInfo(sender.jid, (users) => {
+        console.log("Received JID info")
+        console.log(users)
+    })
     console.log(`GROUP:${group.code}: [${sender.displayName}]: ${msg}`)
+    Kik.sendGroupMessage(group.jid, msg, (delivered, read) => {
+        if(delivered){
+            console.log("groupdelivered" + delivered)
+        }else if(read){
+            console.log("groupread" + read)
+        }
+    })
 })
 Kik.on("grouptyping", (group, sender, isTyping) => {
     if(isTyping){
@@ -37,9 +51,15 @@ Kik.on("userleftgroup", (group, user, kickedBy) => {
 Kik.on("userjoinedgroup", (group, user, invitedBy) => {
     console.log(`GROUP:${group.code}: ${user} joined the group`)
 })
-
 //private messaging events
 Kik.on("receivedprivatemsg", (sender, msg) => {
+    Kik.sendPrivateMessage(sender.jid, msg, (delivered, read) => {
+        if(delivered){
+            console.log("pmdelivered" + delivered)
+        }else if(read){
+            console.log("pmread" + read)
+        }
+    })
     console.log(`PRIVATE: [${sender.displayName}]: ${msg}`)
 })
 

8f515e9c21c534d6eba2d77ca61b0e6708b6080b
Yassien <11986226+YassienW@users.noreply.github.com>
2019-01-05 16:39:48 +0200

    Grouped handlers that return a user together

--- src/handlers/messageHandler.js
+++ src/handlers/messageHandler.js
@@ -2,8 +2,8 @@ module.exports = (client, callbacks, id, data) => {
     let type = data.find("message").attrs.type
 
     if(type === "groupchat"){
-         let group = client.groups.find((group) => {return group.jid === data.find("g").attrs.jid})
-        let user = null//client.users.find((user) => {return user.jid === data.find("message").attrs.from})
+        let group = client.groups.find((group) => {return group.jid === data.find("g").attrs.jid})
+        let user = client.users.find((user) => {return user.jid === data.find("message").attrs.from})
 
         if(data.find("body")){
             client.emit("receivedgroupmsg", group, user, data.find("body").text)
@@ -27,45 +27,41 @@ module.exports = (client, callbacks, id, data) => {
         }else if(data.find("images")){
             client.emit("receivedgroupimg", group, user, client.imgManager.downloadImg(data.find("file-url").text))
         }
-    }else if(type === "chat"){
-        if(data.find("xiphias-mobileremote-call")){
-            //safetynet message
+    }else if(type === "chat" || type === "is-typing"){
+        let jid = data.find("message").attrs.from
 
-        }else if(data.find("body")){
-            let jid = data.find("message").attrs.from
+        let user = {jid: jid, username: null, displayName: null}
+        if(client.params.trackFriendInfo /*|| client.params.trackUserInfo*/){
+            //try to find the user data in friends first
+            let userSearch = client.friends.find(friend => {return friend.jid === jid})
+            user = (userSearch? userSearch : user)
 
-            let user = {jid: jid, username: null, displayName: null}
-            if(client.params.trackFriendInfo /*|| client.params.trackUserInfo*/){
-                //try to find the user data in friends first
-                let userSearch = client.friends.find(friend => {return friend.jid === jid})
-
-                user = (userSearch? userSearch : user)
-
-                /*if(!user){
-                      //try to find the user data in users
-                      user = client.users.find(user => {return user.jid === jid})
-                      //if we don't find the user in users, and user tracking is on, get his jid info (this automatically
-                      //adds him//to users), otherwise we return what we found
-                      if(!user && client.params.trackUserInfo){
-                          client.getJidInfo(data.find("message").attrs.from, (users) => {
-                              client.emit("receivedprivatemsg", users[0], data.find("body").text)
-                          })
-                      }else{
-                          client.emit("receivedprivatemsg", user, data.find("body").text)
-                      }
+            /*if(!user){
+                  //try to find the user data in users
+                  user = client.users.find(user => {return user.jid === jid})
+                  //if we don't find the user in users, and user tracking is on, get his jid info (this automatically
+                  //adds him//to users), otherwise we return what we found
+                  if(!user && client.params.trackUserInfo){
+                      client.getJidInfo(data.find("message").attrs.from, (users) => {
+                          client.emit("receivedprivatemsg", users[0], data.find("body").text)
+                      })
                   }else{
                       client.emit("receivedprivatemsg", user, data.find("body").text)
-                  }*/
-            }
-            client.emit("receivedprivatemsg", user, data.find("body").text)
-        }else if(data.find("images")){
-            let user = {jid: data.find("message").attrs.from, username: null, displayName: null}
+                  }
+              }else{
+                  client.emit("receivedprivatemsg", user, data.find("body").text)
+              }*/
+        }
 
+        if(data.find("xiphias-mobileremote-call")){
+            //safetynet message
+        }else if(data.find("body")){
+            client.emit("receivedprivatemsg", user, data.find("body").text)
+        }else if(type === "is-typing"){
+            client.emit("privatetyping", user, data.find("is-typing").attrs.val === "true")
+        }else if(data.find("images")){
             client.emit("receivedprivateimg", user, client.imgManager.downloadImg(data.find("file-url").text))
         }
-    }else if(type === "is-typing"){
-        let user = client.friends.find((friend) => {return friend.jid === data.find("message").attrs.from})
-        client.emit("privatetyping", user, data.find("is-typing").attrs.val === "true")
     }else if(type === "receipt"){
         let receipt = data.find("receipt").attrs.type
 

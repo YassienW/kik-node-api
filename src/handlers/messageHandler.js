29094f96eeeca60687f003fdfc73b0ce2ee5eb8b
Yassien <11986226+YassienW@users.noreply.github.com>
2019-01-05 20:21:47 +0200

    Updated the userJoinedGroup and userLeftGroup events
    If trackUserInfo is true, grab the jid then trigger userJoinedGroup in the callback
    userLeftGroup now only searches for user if trackUserInfo is true

--- src/handlers/messageHandler.js
+++ src/handlers/messageHandler.js
@@ -9,28 +9,34 @@ module.exports = (client, callbacks, id, data) => {
             client.emit("receivedgroupmsg", group, user, data.find("body").text)
         }else if(data.find("is-typing")){
             client.emit("grouptyping", group, user, data.find("is-typing").attrs.val === "true")
+        }else if(data.find("images")){
+            client.emit("receivedgroupimg", group, user, client.imgManager.getImg(data.find("file-url").text, false))
         }else if(data.find("status")){
             let status = data.find("status")
 
             if(status.text.includes("left") || status.text.includes("removed")){
-                user = client.users.find((user) => {return user.jid === status.attrs.jid})
                 let kickedBy = (status.text.includes("removed")? status.text.split("has")[0].trim() : null)
 
+                if(client.params.trackUserInfo){
+                    //try to find the user data in friends first
+                    let userSearch = client.friends.find(friend => {return friend.jid === status.attrs.jid})
+                    user = (userSearch? userSearch : user)
+                }
                 client.emit("userleftgroup", group, user, kickedBy)
             }else if(status.text.includes("joined")){
-                client.getJidInfo(data.find("status").attrs.jid)
-
-                //i can't return user info since i have to recieve the getJidInfo response first for a user that just joined,
-                //return the JID until i find a way
-                client.emit("userjoinedgroup", group, data.find("status").attrs.jid, null)
+                if(client.params.trackUserInfo){
+                    client.getJidInfo(data.find("status").attrs.jid,(users) => {
+                        client.emit("userjoinedgroup", group, users[0], null)
+                    })
+                }else{
+                    client.emit("userjoinedgroup", group, user, null)
+                }
             }
-        }else if(data.find("images")){
-            client.emit("receivedgroupimg", group, user, client.imgManager.getImg(data.find("file-url").text, false))
         }
     }else if(type === "chat" || type === "is-typing"){
         let jid = data.find("message").attrs.from
-
         let user = {jid: jid, username: null, displayName: null}
+
         if(client.params.trackFriendInfo /*|| client.params.trackUserInfo*/){
             //try to find the user data in friends first
             let userSearch = client.friends.find(friend => {return friend.jid === jid})

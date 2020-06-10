04bb00c0ebdd48db9e9aab5abe6109d74f7d5afc
Yassien <11986226+YassienW@users.noreply.github.com>
2020-06-10 18:08:54 +0200

    Add img upload functionality

--- src/imgManager.js
+++ src/imgManager.js
@@ -1,10 +1,18 @@
 const fs = require("fs"),
+    config = require("./config"),
+    {blockhashData} = require("blockhash"),
+    cryptoUtils = require("./cryptoUtils"),
+    sharp = require("sharp"),
+    axios = require("axios"),
     https = require("https");
 
 class ImageManager {
-    constructor(username, saveImages){
+    constructor(username, password, node, saveImages){
         this.username = username;
+        this.password = password;
         this.saveImages = saveImages;
+        this.senderJid = `${node}@talk.kik.com`;
+        this.appId = "com.kik.ext.gallery";
 
         if(!fs.existsSync("./images")){
             fs.mkdirSync("./images");
@@ -18,6 +26,44 @@ class ImageManager {
         if(!fs.existsSync(`./images/${this.username}/private`)){
             fs.mkdirSync(`./images/${this.username}/private`);
         }
+    }
+    async uploadImg(imgPath){
+        const image = sharp(imgPath);
+        const buffer = await image.jpeg().toBuffer();
+        const raw = await image.jpeg().raw().toBuffer({resolveWithObject: true});
+
+        const size = buffer.byteLength;
+        const md5 = cryptoUtils.bufferToMd5(buffer);
+        const sha1 = cryptoUtils.bufferToSha1(buffer);
+        const previewBase64 = buffer.toString("base64");
+        const previewSha1 = cryptoUtils.bufferToSha1(buffer);
+        const previewBlockhash = blockhashData({data: raw.data, height: raw.info.height, width: raw.info.width},
+            16, 2);
+
+        const contentId = cryptoUtils.generateUUID();
+        const url = `https://platform.kik.com/content/files/${contentId}`;
+        const headers = {
+            "Host": "platform.kik.com",
+            "Connection": "Keep-Alive",
+            "Content-Length": size,
+            "User-Agent": `Kik/${config.kikVersionInfo.version} (Android 7.1.2) Content`,
+            "x-kik-jid": this.senderJid,
+            "x-kik-password": cryptoUtils.generatePasskey(this.username, this.password),
+            "x-kik-verification": cryptoUtils.generateImageVerification(contentId, this.appId),
+            "x-kik-app-id": this.appId,
+            "x-kik-content-chunks": "1",
+            "x-kik-content-size": size,
+            "x-kik-content-md5": md5,
+            "x-kik-chunk-number": "0",
+            "x-kik-chunk-md5": md5,
+            "x-kik-sha1-original": sha1.toUpperCase(),
+            "x-kik-sha1-scaled": previewSha1.toUpperCase(),
+            "x-kik-blockhash-scaled": previewBlockhash.toUpperCase(),
+            "Content-Type": "image/jpeg",
+            "x-kik-content-extension": ".jpg"
+        };
+        await axios.put(url, buffer, {headers});
+        return {contentId, size, sha1, previewSha1, previewBlockhash, previewBase64};
     }
     //2nd param is used for determining which folder to save in
     getImg(url, isPrivate){
